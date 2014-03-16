7d72e84fd8f86658f26ef9135cc8af57624e5845
mcaudillo <mcaudillo@fullsail.edu>
2014-03-15 23:12:39 -0600

    Accelerometer controls commented and final touches
    Only missing the trigger of the end of the game.

--- GameProject/Game/Assets/Scripts/Trigger.cs
+++ GameProject/Game/Assets/Scripts/Trigger.cs
@@ -10,8 +10,25 @@ using Sfs2X.Logging;
 using Sfs2X.FSM;
 
 public class Trigger : MonoBehaviour {
-	void OnTriggerEnter (Collider other){
-		GUI.Label("YOU WON.");
-		GUI.Button("Return to login");
+	bool atEnd = false;
+	void OnGUI(){
+		if (atEnd) {
+			GUI.BeginGroup(new Rect((Screen.width/2)-50,(Screen.height/2)-60,100,120));
+			GUI.Label (new Rect(0,0,100,20),"You Win!");
+			if(GUI.Button(new Rect(0,20,100,50),"Play Again")){
+				Application.LoadLevel(0);
+			}
+			if(GUI.Button(new Rect(0,70,100,50),"Quit")){
+				Application.Quit();
+			}
+			GUI.EndGroup();
+			           
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.collider.name == "endPoint") {
+			atEnd=true;		
+		}
 	}
 }
