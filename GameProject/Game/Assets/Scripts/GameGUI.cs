8fbe26fc6f028438679b4fbc6696eeaf23ad8ce7
mcaudillo <mcaudillo@fullsail.edu>
2014-03-15 19:53:02 -0600

    new repository
    Smart Fox Server 2X for multiplayer

---
+++ GameProject/Game/Assets/Scripts/GameGUI.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Entities;
+using Sfs2X.Entities.Data;
+using Sfs2X.Entities.Variables;
+using Sfs2X.Requests;
+using Sfs2X.Logging;
+
+
+public class GameGUI : MonoBehaviour {
+	
+	//----------------------------------------------------------
+	// Setup variables
+	//----------------------------------------------------------
+	private GameManager gameManager;
+	
+	//----------------------------------------------------------
+	// Unity callbacks
+	//----------------------------------------------------------
+	void Start() {
+		gameManager = this.gameObject.GetComponent<GameManager>();
+	}
+	
+	void OnGUI() {
+		// We basically just draw some buttons to change color and model of our player
+		GUILayout.BeginArea(new Rect(0, 0, 150, 400));
+		GUILayout.BeginVertical();
+		
+		GUILayout.Label("Follow the music to get the Jade and win.");
+		GUILayout.Label("Color of your Chuen");
+		
+		if (GUILayout.Button("Blue")) {
+			gameManager.ChangePlayerMaterial(0);
+		}
+		
+		if (GUILayout.Button("Green")) {
+			gameManager.ChangePlayerMaterial(1);
+		}
+		
+		if (GUILayout.Button("Magenta")) {
+			gameManager.ChangePlayerMaterial(2);
+		}
+		
+		if (GUILayout.Button("Orange")) {
+			gameManager.ChangePlayerMaterial(3);
+		}
+		
+		GUILayout.EndVertical();
+		GUILayout.EndArea();
+	}
+}
---
+++ GameProject/Game/Assets/Scripts/Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Entities;
+using Sfs2X.Entities.Data;
+using Sfs2X.Entities.Variables;
+using Sfs2X.Requests;
+using Sfs2X.Logging;
+using Sfs2X.FSM;
+
+public class Trigger : MonoBehaviour {
+	void OnTriggerEnter (Collider other){
+		GUI.Label("YOU WON.");
+		GUI.Button("Return to login");
+	}
+}
